2aef7ee38976341014b158a72d0a6293402eb221
Wangstaboi <bubmom9@gmail.com>
2022-10-17 23:48:06 -0700

    added event listeners

--- src/App.jsx
+++ src/App.jsx
@@ -8,12 +8,49 @@ function App() {
 const [minutes, setMinutes] = useState('25');
 const [seconds, setSeconds] = useState('00');
 
-function thing() {
-  
+function startButton() {
+  startTimer();
+  console.log({'Minutes': minutes});
+  console.log({'Seconds': seconds});
+}
+
+function startTimer() {
+ 
+    if (seconds === '00') {
+      const intervalMin = setInterval(() => {
+        setMinutes(decrementMinutes());
+      }, 1000);
+      return () => clearInterval(intervalMin);
+    } else {
+      const intervalSec = setInterval(() => {
+        setSeconds(decrementSeconds());
+      }, 1000);
+      return () => clearInterval(intervalSec);
+    }
+  }
+
+
+
+//Functions that change time
+function decrementMinutes() {
+  let intMinute = Number(minutes);
+  intMinute--;
+  setSeconds('59');
+  return intMinute.toString();
+ 
 }
 
 
+function decrementSeconds() {
+  let intSeconds = Number(seconds);
+  intSeconds--;
+  return intSeconds.toString();
+}
 
+function reset(){
+  setMinutes('25');
+  setSeconds('00');
+}
 
   return (
     <div className="App">
@@ -25,9 +62,9 @@ function thing() {
         <h1 id="seconds" dangerouslySetInnerHTML={{__html: seconds}}></h1>
       </div>
       <div>
-        <button id="start" className='buttons'>Start</button>
+        <button id="start" className='buttons' onClick={startButton}>Start</button>
         <button id="pause" className='buttons'>Pause</button>
-        <button id="reset" className='buttons'>Reset</button>
+        <button id="reset" className='buttons' onClick={reset}>Reset</button>
       </div>
     </div>
   );
